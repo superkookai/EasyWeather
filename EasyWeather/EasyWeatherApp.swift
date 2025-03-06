1be4f56e118781dd6066caf182f3e9c5f0390a5a
superkookai <weerawut@live.com>
2025-03-06 12:48:16 +0700

    Initial Commit

---
+++ EasyWeather/EasyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyWeatherApp.swift
+//  EasyWeather
+//
+//  Created by Weerawut Chaiyasomboon on 06/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct EasyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
